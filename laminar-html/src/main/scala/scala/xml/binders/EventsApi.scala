cf6e66a9338939ac2b493945a467d9e9036e2590
elgca <modtekent@live.com>
2025-01-23 10:21:35 +0800

    支持设置监听事件的Options

--- laminar-html/src/main/scala/scala/xml/binders/EventsApi.scala
+++ laminar-html/src/main/scala/scala/xml/binders/EventsApi.scala
@@ -2,52 +2,101 @@ package scala.xml
 package binders
 
 import org.scalajs.dom
+import org.scalajs.dom.EventListenerOptions
 
 import scala.scalajs.js
 
 object EventsApi {
   type JsListener = js.Function1[? <: dom.Event, Unit]
 
-  trait ToJsListener[T] {
+  trait ListenerConversion[T] {
     def apply(fun: T): JsListener
   }
 
-  object ToJsListener {
-    given dynamic: ToJsListener[js.Dynamic => Unit] = fun => (e: dom.Event) => fun(e.asInstanceOf[js.Dynamic])
+  object ListenerConversion {
+    given dynamic: ListenerConversion[js.Dynamic => Unit] = fun => (e: dom.Event) => fun(e.asInstanceOf[js.Dynamic])
 
-    given unit: ToJsListener[() => Unit] = fun => (e: dom.Event) => fun()
+    given unit: ListenerConversion[() => Unit] = fun => (e: dom.Event) => fun()
 
-    given event[Event <: dom.Event]: ToJsListener[Event => Unit] = fun => fun
+    given event[Event <: dom.Event]: ListenerConversion[Event => Unit] = fun => fun
 
-    given str: ToJsListener[String => Unit] = fun => { (ev: dom.Event) =>
+    given str: ListenerConversion[String => Unit] = fun => { (ev: dom.Event) =>
       fun(DomApi.getValue(ev.target.asInstanceOf[dom.Element]).getOrElse(""))
     }
 
-    given checked: ToJsListener[Boolean => Unit] = fun => { (ev: dom.Event) =>
+    given checked: ListenerConversion[Boolean => Unit] = fun => { (ev: dom.Event) =>
       fun(DomApi.getChecked(ev.target.asInstanceOf[dom.Element]).getOrElse(false))
     }
 
-    given file: ToJsListener[List[dom.File] => Unit] = fun => { (ev: dom.Event) =>
+    given file: ListenerConversion[List[dom.File] => Unit] = fun => { (ev: dom.Event) =>
       fun(DomApi.getFiles(ev.target.asInstanceOf[dom.Element]).getOrElse(List.empty))
     }
 
+  }
+
+  class EventListener(
+    val options: dom.EventListenerOptions,
+    val callback: JsListener,
+  )
+
+  trait ToJsListener[T] {
+    def apply(fun: T): EventListener
+  }
+
+  object ToJsListener {
+    //      Listener[Event] | //
+    //      (dom.EventListenerOptions, Listener[Event]) | //
+    //      (Listener[Event] | dom.EventListenerOptions)
+
+    // 用于校验的类型信息
     type ListenerFuncTypes[Event <: dom.Event] = (() => Unit) | //
       ((? >: Event) => Unit) | //
       (String => Unit) | //
       (Boolean => Unit) | //
       (List[dom.File] => Unit) | //
-      (js.Dynamic => Unit)
+      (js.Dynamic => Unit) | //
+      (dom.EventListenerOptions, (() => Unit)) | //
+      (dom.EventListenerOptions, ((? >: Event) => Unit)) | //
+      (dom.EventListenerOptions, (String => Unit)) | //
+      (dom.EventListenerOptions, (Boolean => Unit)) | //
+      (dom.EventListenerOptions, (List[dom.File] => Unit)) | //
+      (dom.EventListenerOptions, (js.Dynamic => Unit)) | //
+      ((() => Unit), dom.EventListenerOptions) | //
+      (((? >: Event) => Unit), dom.EventListenerOptions) | //
+      ((String => Unit), dom.EventListenerOptions) | //
+      ((Boolean => Unit), dom.EventListenerOptions) | //
+      ((List[dom.File] => Unit), dom.EventListenerOptions) | //
+      ((js.Dynamic => Unit), dom.EventListenerOptions)
+
+    given noneOptions[T: ListenerConversion]: ToJsListener[T] = (fun: T) =>
+      EventListener(
+        new EventListenerOptions { capture = false; passive = false },
+        summon[ListenerConversion[T]].apply(fun))
+
+    given withOptions[T: ListenerConversion]: ToJsListener[(dom.EventListenerOptions, T)] =
+      (tuple: (dom.EventListenerOptions, T)) =>
+        EventListener(
+          tuple._1,
+          summon[ListenerConversion[T]].apply(tuple._2),
+        )
+
+    given withOptions2[T: ListenerConversion]: ToJsListener[(T, dom.EventListenerOptions)] =
+      (tuple: (T, dom.EventListenerOptions)) =>
+        EventListener(
+          tuple._2,
+          summon[ListenerConversion[T]].apply(tuple._1),
+        )
   }
 
   def addEventListener(
     name: String,
-    listener: JsListener,
+    listener: EventListener,
   ): MetatDataBinder = (ns, element) => {
     val subscribe = (ctx: MountContext[ReactiveElement.Base]) => {
-      element.ref.addEventListener(name, listener)
+      element.ref.addEventListener(name, listener.callback, listener.options)
       new Subscription(
         ctx.owner,
-        cleanup = () => { element.ref.removeEventListener(name, listener) },
+        cleanup = () => { element.ref.removeEventListener(name, listener.callback, listener.options); },
       )
     }
 
@@ -60,10 +109,10 @@ object EventsApi {
     conversion: ToJsListener[T],
   ): MetatDataBinder = (ns, element) => {
     ReactiveElement.bindSubscriptionUnsafe(element) { c =>
-      var before: Option[JsListener] = None
-      def clearBefore()              = before match {
+      var before: Option[EventListener] = None
+      def clearBefore()                 = before match {
         case Some(beforeListener) =>
-          element.ref.removeEventListener(name, beforeListener)
+          element.ref.removeEventListener(name, beforeListener.callback, beforeListener.options)
           before = None
         case None                 =>
       }
@@ -71,7 +120,7 @@ object EventsApi {
       listener.toObservable.foreach { nextValue =>
         val listener = conversion.apply(nextValue)
         clearBefore()
-        element.ref.addEventListener(name, listener)
+        element.ref.addEventListener(name, listener.callback, listener.options)
         before = Some(listener)
       }(c.owner)
 
